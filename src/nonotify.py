39ad2f49feefac4227f27b8d3931a3fc5b255746
David Lyle <dlyle65535@gmail.com>
2017-11-06 17:16:33 -0500

    Refactor for better flow and EAT_ALL notify commands - xchat doesn't understand them.

--- src/nonotify.py
+++ src/nonotify.py
@@ -14,7 +14,7 @@ def on_unload(userdata):
     xchat.prnt('%s%s has been unloaded.' % (green, __module_name__))
 
 
-def nonotify_callback(word, word_eol, user_data):
+def nonotify_callback(word):
     if xchat.get_info("channel") in channel_list:
         xchat.emit_print('Channel Message','%s%s' % (red, word[0]),'%s%s' % (red,word[1]))
         return xchat.EAT_ALL
@@ -22,19 +22,20 @@ def nonotify_callback(word, word_eol, user_data):
         return xchat.EAT_NONE
 
 
-def remove_nonotify(word,word_eol,userdata):
-    if len(word) > 2 and word[2] in channel_list:
-        xchat.prnt('%sNoNotify - Removing %s from NoNotify channel list.' % (green, word[2]))
-        channel_list.remove(word[2])
+def remove_nonotify(channel):
+    if channel in channel_list:
+        xchat.prnt('%sNoNotify - Removing %s from NoNotify channel list.' % (green, channel))
+        channel_list.remove(channel)
     else:
-        xchat.prnt('%sNoNotify - Not removing %s - not in list.' % (green, word[2]))
+        xchat.prnt('%sNoNotify - Not removing %s - not in list.' % (green, channel))
+    return xchat.EAT_ALL
 
 
 def nonotify(word,word_eol,userdata):
     if word[1] == 'show':
-        return show_nonotify(word,word_eol,userdata)
+        return show_nonotify()
     if word[1] == 'remove':
-        return remove_nonotify(word,word_eol,userdata)
+        return remove_nonotify(word[2])
     channels = re.split('(?:,|\s)+',word_eol[1])
     for channel in channels:
         if channel not in channel_list:
@@ -42,15 +43,17 @@ def nonotify(word,word_eol,userdata):
            channel_list.append(channel)
         else:
            xchat.prnt('%sNoNotify -  Not adding %s, already in NoNotify channel list.' % (green,channel))
+    return xchat.EAT_ALL
 
 
-def show_nonotify(word,word_eol,userdata):
+def show_nonotify():
     if not channel_list:
         xchat.prnt('%sNoNotify - The NoNotify list is empty.' % green)
     else:
         xchat.prnt('%sNoNotify - The following channels are on the NoNotify list:' % green)
         for channel in channel_list:
             xchat.prnt('%s    - %s' % (green, channel))
+    return xchat.EAT_ALL
 
 xchat.hook_print('Channel Msg Hilight', nonotify_callback)
 xchat.hook_print('Channel Action Hilight', nonotify_callback)
